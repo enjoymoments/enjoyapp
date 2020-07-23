4201d90d416d66b7771724f2ca7925888d229808
Fliper <fliper@Flipers-MacBook-Pro.local>
2020-07-22 22:13:21 -0300

    color test

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const _primary = 0xff281445;
+  //static const _primary = 0xFF42157A;
   static const primary = Color(_primary);
   static const secundary = Color(0xffffffff);
   static const menuColor = Color(0xFFfafafa);
