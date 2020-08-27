9c213f284088023e46703ad85e3fd68525d9d480
Felipe Silva <felipequefez@gmail.com>
2020-08-26 23:23:37 -0300

    change color

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Color(0xff281445);
+  static const primary = Color(0xff00A99D);
   static const secundary = Color(0xffffffff);
   static const menuColor = Color(0xFFfafafa);
   static const iconsColor = Color(0xFF737373);
