87c542ee2daab3148402ef7fb0d7317a13ca9404
Felipe Silva <felipequefez@gmail.com>
2020-09-10 20:45:20 -0300

    in development

--- lib/views/places/widgets/place_card_item.dart
+++ lib/views/places/widgets/place_card_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mozin/views/places/place_item_details.dart';
 import 'package:mozin/views/shared/custom_badge.dart';
 import 'package:mozin/views/shared/extension.dart';
 import 'package:mozin/views/shared/spacer_box.dart';
@@ -6,45 +7,54 @@ import 'package:mozin/views/shared/spacer_box.dart';
 class PlaceCardItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding:
-          const EdgeInsets.only(top: 20.0, bottom: 20.0, left: 5.0, right: 5.0),
-      decoration: myBoxDecoration(context),
-      child: Row(
-        children: [
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              "Gelato".title(context),
-              SpacerBox.v8,
-              Row(
-                children: [
-                  CustomBadge(
-                    child: _buildRating(context),
-                  ),
-                  SpacerBox.h8,
-                  CustomBadge(
-                    child: 'Sorvetes'.description(
-                      context,
-                      color: Theme.of(context).iconTheme.color,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => PlaceItemDetails(),
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(
+            top: 20.0, bottom: 20.0, left: 5.0, right: 5.0),
+        decoration: myBoxDecoration(context),
+        child: Row(
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                "Gelato".title(context),
+                SpacerBox.v8,
+                Row(
+                  children: [
+                    CustomBadge(
+                      child: _buildRating(context),
                     ),
-                  ),
-                ],
-              ),
-              SpacerBox.v8,
-              "R. Doces lembranças, 123 - Vila Eternize,  São Paulo - SP, 04310-040"
-                  .description(context,
-                      maxLines: 2, color: Theme.of(context).iconTheme.color),
-            ],
-          ),
-          Expanded(
-            child: Image.asset(
-              'assets/images/ice_cream.png',
-              fit: BoxFit.fill,
+                    SpacerBox.h8,
+                    CustomBadge(
+                      child: 'Sorvetes'.description(
+                        context,
+                        color: Theme.of(context).iconTheme.color,
+                      ),
+                    ),
+                  ],
+                ),
+                SpacerBox.v8,
+                "R. Doces lembranças, 123 - Vila Eternize,  São Paulo - SP, 04310-040"
+                    .description(context,
+                        maxLines: 2, color: Theme.of(context).iconTheme.color),
+              ],
             ),
-          ),
-        ],
+            Expanded(
+              child: Image.asset(
+                'assets/images/ice_cream.png',
+                fit: BoxFit.fill,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -62,7 +72,10 @@ class PlaceCardItem extends StatelessWidget {
       children: [
         "4,3".description(context, color: Theme.of(context).iconTheme.color),
         SpacerBox.h5,
-         Icon(Icons.star, size: 10,),
+        Icon(
+          Icons.star,
+          size: 10,
+        ),
         SpacerBox.h5,
         "(1.568)"
             .description(context, color: Theme.of(context).iconTheme.color),
