35a5ec604c5e5d6fdae0d3e72906450f7f227880
Felipe Silva <felipequefez@gmail.com>
2020-09-22 21:45:37 -0300

    implemented week

--- lib/views/places/widgets/tabs/general/place_available_widget.dart
+++ lib/views/places/widgets/tabs/general/place_available_widget.dart
@@ -1,3 +1,4 @@
+import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 import 'package:mozin/views/shared/custom_border.dart';
 import 'package:mozin/views/shared/custom_container.dart';
@@ -5,28 +6,76 @@ import 'package:mozin/views/shared/spacer_box.dart';
 import 'package:mozin/views/shared/extension.dart';
 
 class PlaceAvailableWidget extends StatelessWidget {
+  final ExpandableController controller = ExpandableController();
+
   @override
   Widget build(BuildContext context) {
-    return CustomBorder(
-      child: CustomContainer(
-        child: Row(
-          children: [
-            Icon(
-              Icons.timer,
-              color: Theme.of(context).primaryColor,
-            ),
-            SpacerBox.h8,
-            Expanded(
-              child:
-                  "Aberto"
-                      .label(
-                context,
-                color: Theme.of(context).primaryColor
-              ),
-            ),
-          ],
+    return InkWell(
+      onTap: () {
+        controller.toggle();
+      },
+      child: CustomBorder(
+        child: CustomContainer(
+          child: ExpandablePanel(
+            controller: controller,
+            header: _buildCollapsed(context),
+            expanded: _buildExpanded(context),
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _buildCollapsed(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          Icons.timer,
+          color: Theme.of(context).primaryColor,
+        ),
+        SpacerBox.h8,
+        "Aberto".label(context, color: Theme.of(context).primaryColor),
+        SpacerBox.h8,
+        "Fecha às 22:30".label(context),
+      ],
+    );
+  }
+
+  Widget _buildExpanded(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        'Horário'.title(context),
+        SpacerBox.h16,
+        Column(
+          children: [
+            ..._buildWeek(context),
+          ],
+        ),
+      ],
+    );
+  }
+
+  List<Widget> _buildWeek(BuildContext context) {
+    return [
+      _buildDay(context, day: "Segunda-feira", hour: "09:30 - 22:00"),
+      _buildDay(context, day: "Segunda-feira", hour: "09:30 - 22:00"),
+      _buildDay(context, day: "Segunda-feira", hour: "09:30 - 22:00"),
+      _buildDay(context, day: "Segunda-feira", hour: "09:30 - 22:00"),
+      _buildDay(context, day: "Segunda-feira", hour: "09:30 - 22:00"),
+      _buildDay(context, day: "Segunda-feira", hour: "09:30 - 22:00"),
+      _buildDay(context, day: "Segunda-feira", hour: "09:30 - 22:00"),
+    ];
+  }
+
+  Widget _buildDay(BuildContext context,
+      {@required String day, @required String hour}) {
+    return Row(
+      children: [
+        day.label(context),
+        SpacerBox.h8,
+        hour.label(context),
+      ],
     );
   }
 }
