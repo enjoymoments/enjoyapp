0e7bf788c262bf5491904d7a7d8b7657bd127664
Felipe Silva <felipequefez@gmail.com>
2021-01-25 02:44:39 -0300

    url endpoint dynamic

--- lib/modules/config/setup.dart
+++ lib/modules/config/setup.dart
@@ -94,10 +94,7 @@ void _setupRemoteClientRepository() {
             url:
                 //'https://localhost:5001/graphql',
                 //'https://10.0.2.2:5001/graphql',
-                //'https://7f97fbadedcd.ngrok.io/graphql',
-                //'https://232b78d44f0b.ngrok.io/graphql/',
-                'https://f2f3641b93ef.ngrok.io/graphql/',
-            //getItInstance<RemoteConfig>().getString(url_endpoint),
+                getItInstance<RemoteConfig>().getString(url_endpoint),
             loggerService: getItInstance<LoggerService>(),
           ));
 }
