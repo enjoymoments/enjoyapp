f127c2f588fd5d7182bccde8a0766d2802337ea7
Felipe Silva <felipequefez@gmail.com>
2021-02-21 15:36:32 -0300

    config setup url

--- lib/modules/config/setup.dart
+++ lib/modules/config/setup.dart
@@ -107,9 +107,9 @@ void _setupRemoteClientRepository() {
       () => RemoteClientRepository(
             dio: _dio,
             url:
-                'https://localhost:5001/graphql',
+                //'https://localhost:5001/graphql',
                 //'https://10.0.2.2:5001/graphql',
-                //getItInstance<RemoteConfig>().getString(url_endpoint),
+                getItInstance<RemoteConfig>().getString(url_endpoint),
             loggerService: getItInstance<LoggerService>(),
           ));
 }
