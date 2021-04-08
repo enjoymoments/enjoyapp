42394b0f2d5fe1a5e312e4063c5d8fad95732c1e
Felipe Silva <felipequefez@gmail.com>
2021-04-08 00:44:54 -0300

    created notification item

--- lib/features/notifications/presentation/pages/notifications_screen.dart
+++ lib/features/notifications/presentation/pages/notifications_screen.dart
@@ -1,29 +1,50 @@
-import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:mozin/package_view/custom_avatar.dart';
 import 'package:mozin/package_view/custom_container.dart';
+import 'package:mozin/package_view/custom_divider.dart';
 import 'package:mozin/package_view/extension.dart';
+import 'package:mozin/package_view/spacer_box.dart';
+
 class NotificationsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomContainer(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          _buildAnimation(),
-          "Ops... ainda não está pronto.".labelIntro(context),
-        ],
+      child: ListView.separated(
+        separatorBuilder: (context, index) => _buildSeparator(),
+        itemCount: 5,
+        itemBuilder: (context, index) => _buildNotificationItem(context),
       ),
     );
   }
 
-  Widget _buildAnimation() {
-    return Container(
-      height: 200,
-      child: FlareActor("assets/animations/watermelon.flr",
-          alignment: Alignment.center,
-          fit: BoxFit.contain,
-          animation: "falling seed"),
+  Widget _buildSeparator() {
+    return Column(
+      children: [
+        SpacerBox.v16,
+        CustomDivider(),
+        SpacerBox.v16,
+      ],
+    );
+  }
+
+  Widget _buildNotificationItem(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        print('tapped');
+      },
+      child: Row(
+        children: [
+          CustomAvatar(),
+          SpacerBox.h16,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              'Joice adicionou uma nova foto'.title(context),
+              '2 minutos atrás'.description(context),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
