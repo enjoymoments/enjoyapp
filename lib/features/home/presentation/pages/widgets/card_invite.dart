601fbf61e92e3303918356edab7eb42922ebef08
Felipe Silva <felipequefez@gmail.com>
2021-04-15 23:10:57 -0300

    fixed card invite in state loading

--- lib/features/home/presentation/pages/widgets/card_invite.dart
+++ lib/features/home/presentation/pages/widgets/card_invite.dart
@@ -6,72 +6,76 @@ import 'package:mozin/features/invite/presentation/bloc/invite_cubit.dart';
 import 'package:mozin/modules/config/setup.dart';
 import 'package:mozin/modules/config/size_config.dart';
 import 'package:mozin/package_view/AppIcons.dart';
-import 'package:mozin/package_view/custom_icon.dart';
-import 'package:mozin/package_view/rounded_loading_button.dart';
 
-//TODO:share review this
 class CardInvite extends StatefulWidget {
   @override
   _CardInviteState createState() => _CardInviteState();
 }
 
 class _CardInviteState extends State<CardInvite> {
-  RoundedLoadingButtonController _actionButtoncontroller;
   InviteCubit _inviteCubit;
+  InviteState _state;
 
   @override
   void initState() {
-    _actionButtoncontroller = RoundedLoadingButtonController();
     _inviteCubit = getItInstance<InviteCubit>();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return CardContainer(
-      child: _buildContent(),
+    return GestureDetector(
+      onTap: () {
+        if (_state != null && !_state.isLoading) {
+          _inviteCubit.generateShareUrl();
+        }
+      },
+      child: CardContainer(
+        child: _buildContent(),
+      ),
     );
   }
 
   Widget _buildContent() {
     return BlocConsumer<InviteCubit, InviteState>(
       cubit: _inviteCubit,
-      listener: (consumerContext, state) {
-        if (state.isSuccess) {
-          _actionButtoncontroller.stop();
-        }
-      },
+      listener: (consumerContext, state) {},
       builder: (context, state) {
-        if (!state.isLoading) {
-          return GestureDetector(
-            onTap: () {
-              _inviteCubit.generateShareUrl();
-              //_actionButtoncontroller.start();
-            },
-            child: CardContent(
-              iconData: AppIcons.inbox,
-              name: 'Convidar meu par',
-              description: 'O que pretende fazer nos próximos dias ?',
-            ),
-          );
-        }
-
-        return _buildLoading();
+        _state = state;
+        return AnimatedSwitcher(
+          duration: Duration(milliseconds: 200),
+          child: _buildState(state),
+        );
       },
     );
   }
 
   Widget _buildLoading() {
-    return RoundedLoadingButton(
-      width: SizeConfig.sizeByPixel(50),
-      controller: _actionButtoncontroller,
-      child: CustomIcon(
-        icon: AppIcons.check,
-        color: Theme.of(context).appBarTheme.iconTheme.color,
+    return ButtonTheme(
+      child: IconButton(
+        padding: EdgeInsets.all(0),
+        icon: SizedBox(
+          height: SizeConfig.sizeByPixel(24.0),
+          width: SizeConfig.sizeByPixel(24.0),
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            strokeWidth: SizeConfig.sizeByPixel(2.0),
+          ),
+        ),
+        onPressed: null,
       ),
-      // onPressed: () {
-      //   _actionButtoncontroller.stop();
-      // },
     );
+  }
+
+  Widget _buildState(InviteState state) {
+    if (!state.isLoading) {
+      return CardContent(
+        iconData: AppIcons.inbox,
+        name: 'Convidar meu par',
+        description: 'O que pretende fazer nos próximos dias ?',
+      );
+    }
+
+    return _buildLoading();
   }
 }
