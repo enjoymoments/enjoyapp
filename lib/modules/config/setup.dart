ff08d5b688d3c9156019d28047d6509fda6fb8ed
Felipe Silva <felipequefez@gmail.com>
2021-06-02 00:24:03 -0300

    remote url api

--- lib/modules/config/setup.dart
+++ lib/modules/config/setup.dart
@@ -136,8 +136,8 @@ void _setupRemoteClientRepository() {
             dio: _dio,
             url: 
             //'https://localhost:5001/graphql',
-            'https://10.0.2.2:5001/graphql',
-            //getItInstance<RemoteConfig>().getString(url_endpoint),
+            //'https://10.0.2.2:5001/graphql',
+            getItInstance<RemoteConfig>().getString(url_endpoint),
             loggerService: getItInstance<LoggerService>(),
           ));
 }
