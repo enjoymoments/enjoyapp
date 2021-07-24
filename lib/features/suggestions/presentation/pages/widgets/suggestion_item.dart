62a8d969d53745e4701a4d0d972476273f68cd56
Felipe Silva <felipequefez@gmail.com>
2021-07-24 01:53:33 -0300

    refactor layout suggestion list

--- lib/features/suggestions/presentation/pages/widgets/suggestion_item.dart
+++ lib/features/suggestions/presentation/pages/widgets/suggestion_item.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:mozin/features/suggestions/data/models/suggestions_model.dart';
+import 'package:mozin/modules/config/size_config.dart';
 import 'package:mozin/package_view/AppIcons.dart';
 import 'package:mozin/package_view/custom_icon.dart';
+import 'package:mozin/package_view/custom_item_modal_fit.dart';
+import 'package:mozin/package_view/custom_modal_fit.dart';
 import 'package:mozin/package_view/extension.dart';
 import 'package:mozin/package_view/spacer_box.dart';
 
@@ -15,21 +19,54 @@ class SuggestionItem extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        item.title.title(context),
+        Row(
+          children: [
+            Expanded(child: item.title.title(context)),
+            _buildOptions(context),
+          ],
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            item.description.description(context),
-            Row(
-              children: [
-                _buildLike(context),
-                SpacerBox.h43,
-                _buildDislike(context),
-              ],
-            ),
+            Expanded(child: item.description.description(context)),
+          ],
+        ),
+        SpacerBox.v8,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            _buildLike(context),
+            SpacerBox.h43,
+            _buildDislike(context),
           ],
         ),
       ],
+    );
+  }
+
+  Widget _buildOptions(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        showMaterialModalBottomSheet(
+          context: context,
+          builder: (context) => CustomModalFit(
+            items: [
+              CustomItemModalFit(
+                text: 'Deletar',
+                iconData: AppIcons.trash,
+                onTap: () {
+                },
+              ),
+            ],
+          ),
+        );
+      },
+      child: Container(
+        width: SizeConfig.sizeByPixel(60),
+        height: SizeConfig.sizeByPixel(30),
+        alignment: Alignment.centerRight,
+        child: CustomIcon(icon: AppIcons.ellipsis_h),
+      ),
     );
   }
 
@@ -38,9 +75,12 @@ class SuggestionItem extends StatelessWidget {
       onTap: () {},
       child: Row(
         children: [
-          CustomIcon(icon: AppIcons.thumbs_up),
+          CustomIcon(
+            icon: AppIcons.thumbs_up,
+            size: 15,
+          ),
           SpacerBox.h5,
-          item.like.toString().label(context),
+          item.like.toString().label(context, fontSize: 16),
         ],
       ),
     );
@@ -51,9 +91,12 @@ class SuggestionItem extends StatelessWidget {
       onTap: () {},
       child: Row(
         children: [
-          CustomIcon(icon: AppIcons.thumbs_down),
+          CustomIcon(
+            icon: AppIcons.thumbs_down,
+            size: 15,
+          ),
           SpacerBox.h5,
-          item.dislike.toString().label(context),
+          item.dislike.toString().label(context, fontSize: 16),
         ],
       ),
     );
