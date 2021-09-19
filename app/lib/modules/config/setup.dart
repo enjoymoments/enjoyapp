a8271ad86b5ec730c03affaeab7aaa31a789f09f
Felipe Silva <felipequefez@gmail.com>
2021-09-19 04:47:11 -0300

    remove trash

--- app/lib/modules/config/setup.dart
+++ app/lib/modules/config/setup.dart
@@ -93,7 +93,6 @@ void _setupRemoteClientRepository() {
                 //'https://localhost:5001/graphql',
             //'https://10.0.2.2:5001/graphql',
             getItInstance<RemoteConfig>().getString(url_endpoint),
-            //'https://3dd0-2804-14c-110-81a4-8090-af11-4055-c3b3.ngrok.io/graphql',
             loggerService: getItInstance<LoggerService>(),
           ));
 }
