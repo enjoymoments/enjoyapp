3873c452045e9c4a76fbf604a093f01e26616813
FelipeQueFez <felipequefez@gmail.com>
2021-11-03 20:32:13 -0300

    link to apple store

--- app/lib/modules/shared/general/services/stores_service.dart
+++ app/lib/modules/shared/general/services/stores_service.dart
@@ -8,6 +8,10 @@ class StoresService {
       LaunchUrlService.launchURL(
           'https://play.google.com/store/apps/details?id=com.codefactory.mozin');
       return;
+    } else if (Platform.isIOS) {
+      LaunchUrlService.launchURL(
+          'https://apps.apple.com/br/app/enjoy-moments/id1570943140');
+      return;
     }
 
     callbackPlatformNotFound();
